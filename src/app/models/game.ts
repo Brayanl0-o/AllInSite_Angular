39cbbc38a1ed70bd81c93b511f36d0d02484831c
Brayan <brayantandap@gmail.com>
2023-10-02 18:59:44 -0500

    Added game interface

---
+++ src/app/models/game.ts
@@ -0,0 +1,10 @@
+export interface Game {
+  id: string;
+  gameName: string;
+  gameImg: string;
+  platform: string;
+  releaseDate: Date;
+  developer: string;
+  genre: string;
+  rating: number;
+}
