ba4ded26c8342039181d6fa7e8ab715399dae7cb
Brayan <brayantandap@gmail.com>
2023-10-02 19:30:23 -0500

    Created request 'getGame'

--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Game } from '../models/game';
+import { enviroment } from 'src/environments/environment.dev';
+import { tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
-  constructor() { }
+  private apiUrl = enviroment.apiUrl
+
+  constructor(private http: HttpClient) { }
+
+  getGame(): Observable<Game[]>{
+    const url= `${this.apiUrl}games/`;
+    return this.http.get<Game[]>(url).pipe(
+      tap((games) => {
+        console.log('Juegos obtenidos:', games);
+      })
+    );
+
+  }
 }
+
