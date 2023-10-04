030f69872277df4d8f4272f6dd6fb5c5a1fc5dce
Brayan <brayantandap@gmail.com>
2023-10-04 14:36:22 -0500

    Updated > aside.component.ts

--- src/app/components/aside/aside.component.ts
+++ src/app/components/aside/aside.component.ts
@@ -9,23 +9,36 @@ import { FilterService } from 'src/app/services/filter/filter.service';
 export class AsideComponent {
 
   selectedFilters = {
-    platforms: [],
-    genres: [],
-    developers: [],
-    highRating: false,
-    lowRating: false
+    platforms: [] as string[], // Define platforms as an array of strings
+    genres: [] as string[]
+
   };
 
   availablePlatforms: string[] = ['PC', 'PlayStation 4', 'Xbox One', 'Nintendo Switch'];
   availableGenres: string[] = ['Sandbox', 'Acción y Aventura', 'Aventura', /* Agrega más géneros aquí */];
-  availableDevelopers: string[] = ['Mojang Studios', 'Rockstar Games', 'Ubisoft', 'Nintendo', /* Agrega más desarrolladores aquí */];
-constructor(private filterService: FilterService){ console.log('filtrSelected from asideC',this.selectedFilters) }
+
+constructor(private filterService: FilterService)
+{ console.log('filtrSelected from asideC',this.selectedFilters) }
 
 applyFilters() {
   // Llama al método updateFilters del servicio FilterService para actualizar los filtros
   this.filterService.updateFilters(this.selectedFilters);
 
 }
+togglePlatform(platform: string) {
+  if (this.selectedFilters.platforms.includes(platform)) {
+    this.selectedFilters.platforms = this.selectedFilters.platforms.filter(p => p !== platform);
+  } else {
+    this.selectedFilters.platforms.push(platform);
+  }
+}
 
+toggleGenre(genre: string) {
+  if (this.selectedFilters.genres.includes(genre)) {
+    this.selectedFilters.genres = this.selectedFilters.genres.filter(g => g !== genre);
+  } else {
+    this.selectedFilters.genres.push(genre);
+  }
+}
 
 }
