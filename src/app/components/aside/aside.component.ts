874740877929b07b5734ea59c91d0510bc35f113
Brayan <brayantandap@gmail.com>
2023-10-03 21:01:47 -0500

    Updated aside.componetnt

--- src/app/components/aside/aside.component.ts
+++ src/app/components/aside/aside.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FilterService } from 'src/app/services/filter/filter.service';
 
 @Component({
   selector: 'app-aside',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./aside.component.css']
 })
 export class AsideComponent {
+
+  selectedFilters = {
+    platforms: [],
+    genres: [],
+    developers: [],
+    highRating: false,
+    lowRating: false
+  };
+
+  availablePlatforms: string[] = ['PC', 'PlayStation 4', 'Xbox One', 'Nintendo Switch'];
+  availableGenres: string[] = ['Sandbox', 'Acción y Aventura', 'Aventura', /* Agrega más géneros aquí */];
+  availableDevelopers: string[] = ['Mojang Studios', 'Rockstar Games', 'Ubisoft', 'Nintendo', /* Agrega más desarrolladores aquí */];
+constructor(private filterService: FilterService){ console.log('filtrSelected from asideC',this.selectedFilters) }
+
+applyFilters() {
+  // Llama al método updateFilters del servicio FilterService para actualizar los filtros
+  this.filterService.updateFilters(this.selectedFilters);
+
+}
+
 
 }
