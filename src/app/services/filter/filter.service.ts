522e686c5e2bda5000c807ed9e3bceff03d7df20
Brayan <brayantandap@gmail.com>
2023-10-03 21:00:30 -0500

    Added Filter

---
+++ src/app/services/filter/filter.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilterService {
+  private selectedFilters = {
+    platforms: [],
+    genres: [],
+    developers: [],
+    highRating: false,
+    lowRating: false
+  };
+
+  private filtersSubject = new BehaviorSubject(this.selectedFilters);
+  filters$ = this.filtersSubject.asObservable();
+
+  updateFilters(newFilters: any) {
+    this.selectedFilters = { ...newFilters };
+    console.log('filter from filerS:', this.selectedFilters)
+    this.filtersSubject.next(this.selectedFilters);
+  }
+}
