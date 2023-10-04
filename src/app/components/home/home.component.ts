7efd524b3e9eb88af4f6bcbc1bb33311009b92d3
Brayan <brayantandap@gmail.com>
2023-10-03 21:00:55 -0500

    Updated home.componente

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HomeService } from 'src/app/services/home.service';
+import { FilterService } from 'src/app/services/filter/filter.service';
 import { Game } from 'src/app/models/game';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,17 +10,55 @@ import { Game } from 'src/app/models/game';
 })
 export class HomeComponent {
   games: Game[] = [];
+  filteredGames: Game[] = [];
 
-  constructor(private homeService: HomeService) {}
+  constructor(
+    private homeService: HomeService,
+    private filterService: FilterService
+  ) {}
 
   ngOnInit(): void {
-  this.loadGameData();
+    this.loadGameData();
+
+    this.filterService.filters$.subscribe((filters) => {
+      this.applyFilters(filters);
+    });
   }
 
-  private loadGameData(){
-    this.homeService.getGame().subscribe(data => {
+  private loadGameData() {
+    this.homeService.getGame().subscribe((data: Game[]) => {
       this.games = data;
-      console.log(data);
+      this.filteredGames = [...this.games];
+      // console.log('dataload',data);
+    });
+  }
+
+  applyFilters(filters: any) {
+    // Aplica los filtros recibidos a los juegos
+    this.filteredGames = this.games.filter((game) => {
+      // Aplica los filtros seleccionados aquí
+      if (filters.platforms.length > 0 && !filters.platforms.includes(game.platform)) {
+        return false;
+      }
+
+      if (filters.genres.length > 0 && !filters.genres.includes(game.genre)) {
+        return false;
+      }
+
+      if (filters.developers.length > 0 && !filters.developers.includes(game.developer)) {
+        return false;
+      }
+
+      if (filters.highRating && game.averageRating === null) {
+        return false;
+      }
+
+      if (filters.lowRating && game.averageRating !== null) {
+        return false;
+      }
+
+      // Si pasa todos los filtros, incluye el juego en la lista filtrada
+      return true;
     });
   }
 }
