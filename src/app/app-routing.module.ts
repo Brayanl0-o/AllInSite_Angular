a4694e769d798211cd86dba4822ebc1584b37487
Brayan <brayantandap@gmail.com>
2023-10-07 01:06:14 -0500

    Updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { VideogamesComponent } from './pages/videogames/videogames.component';
+
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'videogames', component: VideogamesComponent }
+
+  // Ruta wildcard para manejar errores 404
+  // { path: '**', component: PageNotFoundComponent }
 
 ];
 
