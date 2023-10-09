2c46d7bd888ea7b6dac5d9b2d4453725980855c5
Brayan <brayantandap@gmail.com>
2023-10-09 13:35:44 -0500

    Updated

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { HomeService } from 'src/app/services/home/home.service';
 import { FilterService } from 'src/app/services/filter/filter.service';
 import { Game } from 'src/app/models/game';
-
+import { SliderGamesComponent } from 'src/app/components/slider-games/slider-games.component';
+import { SliderGamesInterface } from 'src/app/models/slider-games';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,10 +11,19 @@ import { Game } from 'src/app/models/game';
 })
 export class HomeComponent {
 
-
+  slides: SliderGamesInterface[]= [
+    {url: '../../../assets/img/dantes.jpg',title:'ejemplo'},
+    {url: '../../../assets/img/shadow2.jpg',title:'ejemplo'},
+    {url: '../../../assets/img/half_life_2-black-whit.jpeg',title:'ejemplo'},
+    {url: '../../../assets/img/Mafia1.jpg',title:'ejemplo'},
+    {url: '../../../assets/img/halo.jpg',title:'ejemplo'},
+    {url: '../../../assets/img/madison.png',title:'ejemplo'},
+    {url: '../../../assets/img/shadow.jpg',title:'ejemplo'},
+    {url: '../../../assets/img/half_life_3.jpeg',title:'ejemplo'},
+  ];
   constructor(
-    private homeService: HomeService,
-    private filterService: FilterService
+    // private homeService: HomeService,
+    // private filterService: FilterService
   ) {}
 
   ngOnInit(): void {
