bb02a43ca8677f58e57c63d66b8fa2e8914d730e
Brayan <brayantandap@gmail.com>
2023-10-09 13:34:16 -0500

    configTailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'logo-pink':'#BF245D',
+        'logo-purple': '#110226',
+        'logo-blue': '#0F16A6',
+        'logo-cyan': '#50E8E9',
+        'logo-black':'#0D0D0D',
+        'cyan-d':'#4DEAEC',
+        'purple-d':'#8D295B',
+        'redpink-d':'#4DEAEC'
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
