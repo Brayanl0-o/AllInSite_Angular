4bf1c75ae6bfaa8adda05d71f70e3c0ea986336d
Brayan <brayantandap@gmail.com>
2023-10-10 13:10:45 -0500

    Updated home.component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { HomeService } from 'src/app/services/home/home.service';
-import { FilterService } from 'src/app/services/filter/filter.service';
-import { Game } from 'src/app/models/game';
-import { SliderGamesComponent } from 'src/app/components/slider-games/slider-games.component';
 import { SliderGamesInterface } from 'src/app/models/slider-games';
 @Component({
   selector: 'app-home',
@@ -22,14 +18,8 @@ export class HomeComponent {
     {url: '../../../assets/img/half_life_3.jpeg',title:'ejemplo'},
   ];
   constructor(
-    // private homeService: HomeService,
-    // private filterService: FilterService
   ) {}
 
   ngOnInit(): void {
-
   }
-
-
-
 }
