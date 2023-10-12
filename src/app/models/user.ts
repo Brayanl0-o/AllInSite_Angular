046ddc1353971745e4c907dbc6e3c0f8f5bcfe60
Brayan <brayantandap@gmail.com>
2023-10-12 00:16:33 -0500

    Added model

---
+++ src/app/models/user.ts
@@ -0,0 +1,13 @@
+export interface User{
+  id: string,
+  userName: string,
+  firstName: string,
+  lastName: string,
+  userImg: string,
+  email: string,
+  password: string,
+  phoneNumber: string,
+  country: string,
+  years:number,
+  desriptionUser: string
+}
