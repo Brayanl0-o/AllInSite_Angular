ac3938335e57dfc18f013962597f7be452d612d0
Brayan <brayantandap@gmail.com>
2023-10-11 19:46:36 -0500

    updated routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { VideogamesComponent } from './pages/videogames/videogames.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'videogames', component: VideogamesComponent }
-
+  { path: 'videogames', component: VideogamesComponent },
+  { path: 'register', component: RegisterComponent }
   // Ruta wildcard para manejar errores 404
   // { path: '**', component: PageNotFoundComponent }
 
