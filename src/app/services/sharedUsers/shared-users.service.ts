4d8ac79651141b55ce1bcd9e93c1474f199288f5
Brayan <brayantandap@gmail.com>
2023-10-12 21:07:51 -0500

    Added & updated > shared-users.services

---
+++ src/app/services/sharedUsers/shared-users.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/user';
+import { enviroment } from 'src/environments/environment.dev';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedUsersService {
+  private URL = enviroment.apiUrl
+
+  constructor(private http: HttpClient) { }
+
+  // Metodo para obtener todos los usuarios del backend
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.URL);
+  }
+  // Metodo par aobtener los datos del usuario logeado por su ID
+  getUser(id: string): Observable<User> {
+    const url = `${this.URL}users/${id}`;
+    // console.log('Requesting user data from:', url);
+    return this.http.get<User>(url);
+  }
+
+}
