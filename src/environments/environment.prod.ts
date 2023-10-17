3422f37a08fedcbf1dfef1bb4d057d6a87e8636d
Brayan <brayantandap@gmail.com>
2023-10-17 15:40:40 -0500

    Updated & fixed environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // url for production
 
-export const enviroment = {
+export const environment  = {
   production: true,
   apiUrl: 'https://allin-api-67ck.onrender.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // url for developer
 
-export const enviroment = {
+export const environment  = {
   production: false,
   apiUrl: 'http://localhost:3000/'
 };
