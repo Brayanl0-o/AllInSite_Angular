f0b9b4ba411b6fc45069413c9923175bf44c8ea8
Brayan <brayantandap@gmail.com>
2023-10-19 00:44:04 -0500

    updated request dataUser

--- src/app/services/sharedUsers/shared-users.service.ts
+++ src/app/services/sharedUsers/shared-users.service.ts
@@ -3,12 +3,16 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/models/user';
 import { environment } from 'src/environments/environment';
-
+import { EventEmitter } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
 export class SharedUsersService {
+
   private URL = environment.apiUrl
+
+  $modal = new EventEmitter<any>();
+  userData = new EventEmitter<any>();
 
   constructor(private http: HttpClient) { }
 
@@ -16,6 +20,7 @@ export class SharedUsersService {
   getUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.URL);
   }
+
   // Metodo par aobtener los datos del usuario logeado por su ID
   getUser(id: string): Observable<User> {
     const url = `${this.URL}users/${id}`;
@@ -23,4 +28,15 @@ export class SharedUsersService {
     return this.http.get<User>(url);
   }
 
+  sendUserData(user: any): void {
+    // Emitir datos del user al componente modal
+    this.userData.emit(user);
+    // console.log('dataUser switchService: ', user)
+  }
+
+  updateUser(id: string, userData: any): Observable<any> {
+    const url = `${this.URL}users/update/${id}`;
+    console.log('userData service', userData)
+    return this.http.put(url, userData);
+  }
 }
