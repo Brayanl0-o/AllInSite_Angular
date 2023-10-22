75d30893ef1adb4108e14d1d8e694ec6436551e9
Brayan <brayantandap@gmail.com>
2023-10-21 21:00:04 -0500

    updated load & validation

--- src/app/pages/send-email/send-email.component.ts
+++ src/app/pages/send-email/send-email.component.ts
@@ -10,6 +10,8 @@ export class SendEmailComponent {
   email: string = ''
   isEmailValid: boolean= true;
   isEmailSent:boolean = false;
+  public isLoading: boolean = false;
+  public errorMessage: string = '';
 
   constructor(private resetPasswordService: ResetPasswordService,
     private router: Router){}
@@ -21,25 +23,39 @@ export class SendEmailComponent {
 
   sendEmailResetPassword(){
     console.log(this.isEmailSent)
+
     if(this.validateEmail(this.email)){
+      this.isLoading = true;
       this.isEmailValid = true;
       this.resetPasswordService.sendPaswordLink(this.email)
       .subscribe(
         ()=>{
           this.isEmailSent = true;
           console.log(this.isEmailSent)
+          this.isLoading = false;
 
 
         }, (error)=> {
           console.error('Error sent email to reset password')
+          this.errorMessage = 'Correo no registrado';
+          this.isLoading = false;
+          setTimeout(() => {
+            this.clearErrorMessage();
+          }, 3500);
         }
         )
+
     }else{
       this.isEmailValid = false;
     }
   }
+  clearErrorMessage() {
+    this.errorMessage = '';
+  }
+
   cancel() {
     // Redirige al usuario a la página de inicio
     this.router.navigate(['/home']);
+    this.isLoading = true;
   }
 }
