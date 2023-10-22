26477b56554bbd0b050590d168dcde1ded39e8c1
Brayan <brayantandap@gmail.com>
2023-10-21 21:39:07 -0500

    updated > videogames.component

--- src/app/pages/videogames/videogames.component.ts
+++ src/app/pages/videogames/videogames.component.ts
@@ -12,46 +12,36 @@ import { User } from 'src/app/models/user';
   styleUrls: ['./videogames.component.css']
 })
 export class VideogamesComponent {
+  @Input()user:User | null = null;
+  userId: string| null= null;
   games: Game[] = [];
   filteredGames: Game[] = [];
-
-  // currentPage = 1; // Página actual
-  // pageSize = 9;  // Juegos por página
-  // totalGames = 13;// Total de juegos (necesario para la paginación)
-  // totalPages: number = 2;
-  // pages!:  number[];
-
   loadingData: boolean = true;
 
   constructor(
     private videogamesService: VideogamesService,
     private filterService: FilterService,
     private userShared: SharedUsersService,
-      private authService: AuthService,
-      private route:ActivatedRoute
+    private authService: AuthService,
+    private route:ActivatedRoute
   ) {}
 
   isUserLoggedIn(){
     return this.authService.loggedIn()
   }
 
-  @Input()user:User | null = null;
-  userId: string| null= null;
   dataUser(){
     // Obtiene el ID del usuario logueado desde el servicio de autenticación
     const loggedInUserId = this.authService.getLoggedInUserId();
     console.log('loggedInUserId:', loggedInUserId);
 
     if (loggedInUserId) {
-      // Asignar el userId obtenido al userId del componente
       this.userId = loggedInUserId;
       this.route.paramMap.subscribe(paramMap => {
 
-        // Obtiene el ID de usuario de la URL
           const id = paramMap.get('id');
           console.log('Id Login: ', id)
 
-          // Comprueba si el ID de usuario de la URL coincide con el usuario logueado
           if (id === loggedInUserId) {
             this.userShared.getUser(id).subscribe(data => {
               this.user = data;
@@ -65,17 +55,12 @@ export class VideogamesComponent {
       }
   }
 
-
   ngOnInit(): void {
-    // this.loadGameData(this.currentPage, this.pageSize);
-    // console.log('loadGameDatareceived in HomeComponent:', this.loadGameData);
     this.dataUser()
     this.loadGameData();
-
-    // this.pagination()
-
     this.subscribeFilter()
   }
+
   private loadGameData() {
     this.videogamesService.getGame().subscribe((data: Game[]) => {
       this.games = data;
@@ -84,19 +69,6 @@ export class VideogamesComponent {
       // console.log('dataload',data);
     });
   }
-
-  // private loadGameData(page: number, pageSize: number) {
-  //   // console.log('Loading game data for page', page);
-  //   this.videogamesService.getGame(page, pageSize).subscribe((response: any) => {
-  //     //  console.log('Game data response:', response);
-  //     this.loadingData = false;
-  //     this.games = response.games; // Asigna la matriz de juegos desde la respuesta de la API
-  //     this.totalGames = response.totalGames; // Actualiza el total de juegos
-  //     // this.currentPage = response.currentPage; // Actualiza la página actual
-  //     this.filteredGames = [...this.games];
-  //     // console.log('dataload', response.games);
-  //   });
-  // }
 
   searchTerm: string = '';
   searchGames() {
@@ -110,6 +82,7 @@ export class VideogamesComponent {
     this.searchTerm = searchTerm; // Actualiza el término de búsqueda
     this.searchGames(); // Llama a la función de búsqueda
   }
+
   showPopover = false;
   showPopoverMouseEnter(game:any) {
    game.showPopover = true;
@@ -119,61 +92,11 @@ export class VideogamesComponent {
     game.showPopover = false;
   }
 
-  // pagination(){
-  //   this.currentPage = 1; // Establece la página inicial
-  //   this.pageSize = 9; // Establece el tamaño de página deseado
-
-  //   // Obtener el número total de juegos desde tu servicio o donde sea que lo obtengas
-  //   this.videogamesService.getGame(this.currentPage, this.pageSize).subscribe((response: any) => {
-  //     this.games = response.games;
-  //     this.totalGames = response.totalGames;
-
-  //     // Verifica si totalGames y pageSize son valores válidos antes de calcular totalPages
-  //     if (this.totalGames && this.pageSize) {
-  //       // Calcular totalPages
-  //       this.totalPages = Math.ceil(this.totalGames / this.pageSize);
-  //     }
-  //     if(this.totalPages !== undefined){
-  //       // Crea un array de números de página desde 1 hasta totalPages
-  //       this.pages = Array.from({ length: this.totalPages }, (_, i) => i + 1);
-  //       // console.log('currentPage', this.currentPage,'totalPages', this.totalPages,'pages', this.pages);
-  //     }
-  //   });
-  // }
-
-  // // Función para cambiar de página
-  // onPageChange(page: number) {
-  //   this.currentPage = page;
-  //   this.loadGameData(page, this.pageSize);
-  // }
-
-  // previousPage() {
-  //   if (this.currentPage > 1) {
-  //     this.currentPage--;
-  //     this.loadGameData(this.currentPage, this.pageSize);
-  //   }
-  // }
-
-  // nextPage() {
-  //   if (this.currentPage < this.totalPages) {
-  //     this.currentPage++;
-  //     this.loadGameData(this.currentPage, this.pageSize);
-  //   }
-  // }
-
-  // goToPage(page: number) {
-  //   if (page >= 1 && page <= this.totalPages && page !== this.currentPage) {
-  //     this.currentPage = page;
-  //     this.loadGameData(this.currentPage, this.pageSize);
-  //   }
-  // }
-
   subscribeFilter(){
     this.filterService.filters$.subscribe((filters) => {
       // console.log('Filters received in HomeComponent:', filters);
       this.applyFilters(filters);
     });
-
   }
 
   applyFilters(filters: any) {
@@ -210,7 +133,7 @@ export class VideogamesComponent {
       }
       // Verifica la fecha de lanzamiento
       if (filters.startDate && filters.endDate) {
-        const gameDate = new Date(game.releaseDate); // Suponiendo que la fecha de lanzamiento está en el formato adecuado
+        const gameDate = new Date(game.releaseDate);
         const startDate = new Date(filters.startDate);
         const endDate = new Date(filters.endDate);
 
@@ -221,9 +144,6 @@ export class VideogamesComponent {
           return false; // Excluye el juego si no cumple con el rango de fechas
         }
       }
-
-
-      // Si pasa todos los filtros, incluye el juego en la lista filtrada
       return true;
     });
 
