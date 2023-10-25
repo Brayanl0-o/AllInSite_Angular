bd81fd96890fda6506cd8e4be5a251982ed7b424
Brayan <brayantandap@gmail.com>
2023-10-24 21:42:24 -0500

    updated > details-game.component.ts

--- src/app/pages/details-game/details-game.component.ts
+++ src/app/pages/details-game/details-game.component.ts
@@ -30,15 +30,27 @@ export class DetailsGameComponent {
 
         if (userId) {
           this.gameDetails$ = this.videogameService.getGameById(userId, gameId as string);
-         // Asigna la URL de la imagen del juego directamente desde los detalles del juego
          this.gameDetails$.subscribe((game) => {
-          this.gameImgUrl = game.gameImg; // Asumiendo que la URL de la imagen se encuentra en el objeto Game
+          console.log('Valor de game:', game);
+          if (game && game.gameImg) {
+            this.gameImgUrl = game.gameImg;
+            console.log('Valor de gameImgUrl:', this.gameImgUrl);
+          } else {
+            console.log('Game o gameImg son nulos o indefinidos.');
+            this.gameImgUrl = '';
+          }
         });
         } else {
           this.gameDetails$ = this.videogameService.getGameById('', gameId as string);
           // Asigna la URL de la imagen del juego directamente desde los detalles del juego
           this.gameDetails$.subscribe((game) => {
-            this.gameImgUrl = game.gameImg; // Asumiendo que la URL de la imagen se encuentra en el objeto Game
+            if (game && game.gameImg) {
+              this.gameImgUrl = game.gameImg;
+              console.log('Valor de gameImgUrl:', this.gameImgUrl);
+            } else {
+              console.log('Game o gameImg son nulos o indefinidos.');
+              this.gameImgUrl = '';
+            }// Asumiendo que la URL de la imagen se encuentra en el objeto Game
           });
         }
       }
