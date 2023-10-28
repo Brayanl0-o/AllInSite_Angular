ecebbff83090d0ee9d85d529ade41bfb6042ac54
Brayan <brayantandap@gmail.com>
2023-10-28 18:21:11 -0500

    update router

--- src/app/pages/details-game/details-game.component.ts
+++ src/app/pages/details-game/details-game.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { VideogamesService } from 'src/app/services/videogames/videogames.service';
 import { Game } from 'src/app/models/game';
 import { Observable } from 'rxjs';
-
+import { environment } from 'src/environments/environment';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { User } from 'src/app/models/user';
+const apiUrl = environment.apiUrl
 @Component({
   selector: 'app-details-game',
   templateUrl: './details-game.component.html',
@@ -13,11 +16,29 @@ export class DetailsGameComponent {
   gameDetails$: Observable<Game>;
   gameId: string | null = null;
   gameImgUrl: string | null = null;
-  constructor(private videogameService: VideogamesService, private route: ActivatedRoute) {
+  constructor(private videogameService: VideogamesService,
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router) {
     this.gameDetails$ = new Observable<Game>();
   }
 
+  navigateToVideogames(userId?: string | null) {
+    const route = userId ? `/videogames;id=${userId}` : '/videogames';
+    this.router.navigateByUrl(route);
+  }
+  imageUrl!: string;
+  backVideogames() {
+    const userId = this.authService.getLoggedInUserId();
+    if (userId) {
+      this.navigateToVideogames(userId);
+    } else {
+      this.navigateToVideogames();
+    }
+  }
+
   ngOnInit() {
+    this.imageUrl = `${apiUrl}uploads/videogames/`
     this.loadDataGame();
   }
 
@@ -31,12 +52,12 @@ export class DetailsGameComponent {
         if (userId) {
           this.gameDetails$ = this.videogameService.getGameById(userId, gameId as string);
          this.gameDetails$.subscribe((game) => {
-          console.log('Valor de game:', game);
+          // console.log('Valor de game:', game);
           if (game && game.gameImg) {
             this.gameImgUrl = game.gameImg;
             console.log('Valor de gameImgUrl:', this.gameImgUrl);
           } else {
-            console.log('Game o gameImg son nulos o indefinidos.');
+            console.error('Game o gameImg son nulos o indefinidos.');
             this.gameImgUrl = '';
           }
         });
@@ -46,9 +67,9 @@ export class DetailsGameComponent {
           this.gameDetails$.subscribe((game) => {
             if (game && game.gameImg) {
               this.gameImgUrl = game.gameImg;
-              console.log('Valor de gameImgUrl:', this.gameImgUrl);
+              // console.log('Valor de gameImgUrl:', this.gameImgUrl);
             } else {
-              console.log('Game o gameImg son nulos o indefinidos.');
+              console.error('Game o gameImg son nulos o indefinidos.');
               this.gameImgUrl = '';
             }// Asumiendo que la URL de la imagen se encuentra en el objeto Game
           });
