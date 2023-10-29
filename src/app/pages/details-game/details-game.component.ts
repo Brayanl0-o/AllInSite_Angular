d3be2c8af70bf2aced76ac8cf260713cfffff67a
Brayan <brayantandap@gmail.com>
2023-10-28 19:25:50 -0500

    updated > details-game.component

--- src/app/pages/details-game/details-game.component.ts
+++ src/app/pages/details-game/details-game.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Renderer2} from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { VideogamesService } from 'src/app/services/videogames/videogames.service';
 import { Game } from 'src/app/models/game';
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { User } from 'src/app/models/user';
+
 const apiUrl = environment.apiUrl
 @Component({
   selector: 'app-details-game',
@@ -16,13 +17,23 @@ export class DetailsGameComponent {
   gameDetails$: Observable<Game>;
   gameId: string | null = null;
   gameImgUrl: string | null = null;
-  constructor(private videogameService: VideogamesService,
+  constructor(private videogamesService: VideogamesService,
     private authService: AuthService,
+    private renderer: Renderer2,
     private route: ActivatedRoute,
     private router: Router) {
     this.gameDetails$ = new Observable<Game>();
   }
-
+  ngOnInit() {
+    this.videogamesService.$modal.subscribe((valu) => { this.isModalVisible =valu })
+    this.imageUrl = `${apiUrl}uploads/videogames/`
+    this.loadDataGame();
+  }
+  isModalVisible!: boolean;
+  openModal() {
+    this.isModalVisible = true;
+    this.renderer.setStyle(document.body, 'overflow', 'hidden');
+  }
   navigateToVideogames(userId?: string | null) {
     const route = userId ? `/videogames;id=${userId}` : '/videogames';
     this.router.navigateByUrl(route);
@@ -37,10 +48,6 @@ export class DetailsGameComponent {
     }
   }
 
-  ngOnInit() {
-    this.imageUrl = `${apiUrl}uploads/videogames/`
-    this.loadDataGame();
-  }
 
   loadDataGame() {
     this.route.paramMap.subscribe((params: ParamMap) => {
@@ -50,7 +57,7 @@ export class DetailsGameComponent {
         console.log('game id:', gameId);
 
         if (userId) {
-          this.gameDetails$ = this.videogameService.getGameById(userId, gameId as string);
+          this.gameDetails$ = this.videogamesService.getGameById(userId, gameId as string);
          this.gameDetails$.subscribe((game) => {
           // console.log('Valor de game:', game);
           if (game && game.gameImg) {
@@ -62,7 +69,7 @@ export class DetailsGameComponent {
           }
         });
         } else {
-          this.gameDetails$ = this.videogameService.getGameById('', gameId as string);
+          this.gameDetails$ = this.videogamesService.getGameById('', gameId as string);
           // Asigna la URL de la imagen del juego directamente desde los detalles del juego
           this.gameDetails$.subscribe((game) => {
             if (game && game.gameImg) {
