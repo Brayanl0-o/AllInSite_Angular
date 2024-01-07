f3b71d6f1023864a690510f63742d47ac63c224e
Brayan <brayantandap@gmail.com>
2024-01-06 20:11:58 -0500

    updated > slider-games.ts

--- src/app/components/slider-games/slider-games.component.ts
+++ src/app/components/slider-games/slider-games.component.ts
@@ -8,8 +8,32 @@ import { SliderGamesInterface } from 'src/app/models/slider-games';
 })
 export class SliderGamesComponent {
 
-    @Input() slides:SliderGamesInterface[] =[];
+    // @Input() slides:SliderGamesInterface[] =[];
+    slides: SliderGamesInterface[]= [
+      {url: '../../../assets/img/slider-home/videogames2.webp',title:'videogames2'},
+      {url: '../../../assets/img/slider-home/dantes.webp',title:'dantes'},
+      {url: '../../../assets/img/slider-home/shadow2.webp',title:'shadow2'},
+      {url: '../../../assets/img/slider-home/half_life_2-black-whit.webp',title:'half life 2'},
+      {url: '../../../assets/img/slider-home/Mafia1.webp',title:'Mafia1'},
+      {url: '../../../assets/img/slider-home/halo.webp',title:'halo'},
+      {url: '../../../assets/img/slider-home/madison.webp',title:'madison'},
+      {url: '../../../assets/img/slider-home/shadow.webp',title:'shadow'},
+      {url: '../../../assets/img/slider-home/half_life_3.webp',title:'half life 3'},
+      {url: '../../../assets/img/slider-home/videogames.webp',title:'videogames'},
 
+    ];
+    // imagesLoaded = 0;
+    // imagesReady = false;
+    // handleImageLoad() {
+    //   this.imagesLoaded++;
+
+    //   // Verificar si todas las imágenes están cargadas antes de iniciar la animación
+    //   if (this.imagesLoaded === this.slides.length) {
+    //     // Todas las imágenes están cargadas, ahora puedes iniciar la animación
+    //     // Por ejemplo, puedes configurar una bandera para indicar que las imágenes están listas.
+    //     this.imagesReady = true;
+    //   }
+    // }
 
     currentIndex:number = 0;
     interval:any;
@@ -21,7 +45,7 @@ export class SliderGamesComponent {
     startAutoPlay() {
       this.interval = setInterval(() => {
         this.goToNext();
-      }, 5000); // Cambia la diapositiva cada 5 segundos (ajusta el tiempo según tus necesidades)
+      }, 6000); // Cambia la diapositiva cada 5 segundos (ajusta el tiempo según tus necesidades)
     }
 
     stopAutoPlay() {
