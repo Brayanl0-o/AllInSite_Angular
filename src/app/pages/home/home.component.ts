6fd4d1263809007717b0f0eb33275b4bfc5f30d3
Brayan <brayantandap@gmail.com>
2024-01-06 20:12:57 -0500

    updated > home.component.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { SliderGamesInterface } from 'src/app/models/slider-games';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { environment } from 'src/environments/environment';
 @Component({
@@ -10,20 +9,6 @@ import { environment } from 'src/environments/environment';
 export class HomeComponent {
   public env: string = environment.apiUrl
   userId: string | null = null;
-
-  slides: SliderGamesInterface[]= [
-    {url: '../../../assets/img/slider-home/videogames2.webp',title:'videogames2'},
-    {url: '../../../assets/img/slider-home/dantes.webp',title:'dantes'},
-    {url: '../../../assets/img/slider-home/shadow2.webp',title:'shadow2'},
-    {url: '../../../assets/img/slider-home/half_life_2-black-whit.webp',title:'half life 2'},
-    {url: '../../../assets/img/slider-home/Mafia1.webp',title:'Mafia1'},
-    {url: '../../../assets/img/slider-home/halo.webp',title:'halo'},
-    {url: '../../../assets/img/slider-home/madison.webp',title:'madison'},
-    {url: '../../../assets/img/slider-home/shadow.webp',title:'shadow'},
-    {url: '../../../assets/img/slider-home/half_life_3.webp',title:'half life 3'},
-    {url: '../../../assets/img/slider-home/videogames.webp',title:'videogames'},
-
-  ];
 
   constructor(private authService: AuthService) {}
 
