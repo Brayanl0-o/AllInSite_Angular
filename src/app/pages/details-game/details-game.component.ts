a6b97268dc3a45d0e35736cc1ef579d0b9e01f3a
Brayan <brayantandap@gmail.com>
2024-01-13 20:39:10 -0500

    updated > details-game.component.ts

--- src/app/pages/details-game/details-game.component.ts
+++ src/app/pages/details-game/details-game.component.ts
@@ -14,9 +14,7 @@ const apiUrl = environment.apiUrl
   styleUrls: ['./details-game.component.css']
 })
 export class DetailsGameComponent {
-  gameDetails$: Observable<Game>;
-  gameId: string | null = null;
-  gameImgUrl: string | null = null;
+
   constructor(private videogamesService: VideogamesService,
     private authService: AuthService,
     private renderer: Renderer2,
@@ -25,13 +23,72 @@ export class DetailsGameComponent {
     this.gameDetails$ = new Observable<Game>();
   }
 
+  // @Input() game: Game = {} as Game;
+  gameDetails$: Observable<Game>;
+  // gameId: string | null = null;
+  gameId!: ''
+  gameImgUrl: string | null = null;
+  gameImg = '';
+  userId = '';
+  isEditingImg = false;
+  selectedFile : File | null = null;
+
+
   ngOnInit() {
     this.videogamesService.$modal.subscribe((valu) => { this.isModalVisible =valu })
+    this.gameDetails$ = this.videogamesService.getGameById(this.userId, this.gameId);
     this.imageUrl = `${apiUrl}uploads/videogames/`
     this.loadDataGame();
     this.isAdminOrNot();
   }
 
+  onFileSelectedGame(event: Event):void {
+    const inputElement = event.target as HTMLInputElement;
+      const file = inputElement?.files?.[0];
+      if (file){
+        this.selectedFile = file;
+      }
+  }
+  updatedGameImg(gameImg: File): void {
+    // console.log('Select file', this.selectedFile);
+
+    // Suscríbete a gameDetails$
+    this.gameDetails$.subscribe((game: Game) => {
+      if (!game) {
+        console.error('Error: No hay datos de actualización');
+        return;
+      }
+      // console.log('game id', game._id);
+      this.videogamesService.updatedGameImg(game._id, gameImg).subscribe(
+        (response) => {
+          // Actualiza el gameDetails$ si es necesario
+          this.gameDetails$ = this.videogamesService.getGameById(this.userId, this.gameId);
+          console.log('Datos act con exito:', response);
+          this.loadDataGame()
+        },
+        (error) => {
+          console.error('Error al updated', error);
+        }
+      );
+    });
+
+    this.isEditingImg = false;
+  }
+
+  newGameImg = '';
+  cancelEditImg(){
+    this.newGameImg = this.gameImg;
+    this.isEditingImg = false;
+  }
+
+  isModalImgVisible!: boolean;
+  openModalImg(game: Game | null): void {
+    if (game) {
+      this.isModalVisible = true
+      this.renderer.setStyle(document.body, 'overflow', 'hidden');
+
+    }
+  }
   isAdmin: boolean = false;
   isAdminOrNot(){
     const logginRoles = this.authService.getLoggedUserRole();
