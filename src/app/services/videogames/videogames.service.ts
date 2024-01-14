630e18df16d9de66233aeb0f3fd383a2417fcf3a
Brayan <brayantandap@gmail.com>
2024-01-13 20:38:53 -0500

    updated > videogames.service.ts

--- src/app/services/videogames/videogames.service.ts
+++ src/app/services/videogames/videogames.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Game } from '../../models/game';
 import { environment } from 'src/environments/environment';
-import { tap } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { EventEmitter } from '@angular/core';
 @Injectable({
   providedIn: 'root'
@@ -12,6 +12,7 @@ export class VideogamesService {
 
   private apiUrl = environment.apiUrl
 
+  $modal = new EventEmitter<any>();
 
 
   constructor(private http: HttpClient) { }
@@ -26,18 +27,13 @@ export class VideogamesService {
   // }
   getGame(): Observable<Game[]>{
     const url= `${this.apiUrl}games/`;
-    return this.http.get<Game[]>(url).pipe(
-      tap((games) => {
-        // console.log('Juegos obtenidos:', games);
-      })
-    );
+    return this.http.get<Game[]>(url)
   }
   getGameById(userId: string | null, gameId: string): Observable<Game>{
     const url = `${this.apiUrl}games/${userId}/${gameId}`;
     // console.log('game id S:',gameId, 'user id S:',userId);
     return this.http.get<Game>(url);
   }
-  $modal = new EventEmitter<any>();
 
   createGame(gameData: any, gameImg: File): Observable<any> {
     const url = `${this.apiUrl}games/create`;
@@ -61,30 +57,38 @@ export class VideogamesService {
     return this.http.post(url, formData, httpOptions);
   }
 
-  updateGame(id: string,gameData: any, gameImg: File): Observable<any> {
+  updateGame(id: string,gameData: any): Observable<any> {
     const url = `${this.apiUrl}games/update/${id}`;
-
-    const formData = new FormData();
-    formData.append('gameName', gameData.gameName);
-    formData.append('platform', gameData.platform);
-    formData.append('releaseDate', gameData.releaseDate);
-    formData.append('developer', gameData.developer);
-    formData.append('genre', gameData.genre);
-    formData.append('averageRating', gameData.averageRating);
-    formData.append('descriptionGame', gameData.descriptionGame);
-    if (gameImg) {
-      formData.append('gameImg', gameImg);
-    }
-    console.log('data', gameData.releaseDate, gameData.averageRating)
-    // formData.append('gameImg', gameImg);
-    // console.log('gameData service', formData)
     const httpOptions = {
       headers: new HttpHeaders({
         'x-access-token': localStorage.getItem('token') || '', // Agrega el token al encabezado
       }),
     };
-    return this.http.patch(url, formData, httpOptions);
+    return this.http.patch(url, gameData, httpOptions);
   }
+
+  updatedGameImg(id:string, gameImg: File){
+    const url = `${this.apiUrl}games/updatedGameImg/${id}`;
+    console.log(url)
+    const formData = new FormData()
+    if(gameImg){
+      formData.append('gameImg', gameImg)
+    }
+
+    return this.http.patch(url,formData).pipe(
+      catchError((error) => {
+        console.error('Error en la solicitud:', error);
+        throw error;
+      })
+    );
+  }
+
+  // gameData = new EventEmitter<any>();
+  // sendGameData(game: any): void {
+  //   // Emitir datos del user al componente modal
+  //   this.gameData.emit(game);
+  //   // console.log('datagame switchService: ', game)
+  // }
 
   deleteGame(gameId: string): Observable<any> {
     const url = `${this.apiUrl}games/delete/${gameId}`;
