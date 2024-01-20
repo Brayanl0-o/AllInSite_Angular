202d23f288d83c8ef540c6195be7d804490780c5
Brayan <brayantandap@gmail.com>
2024-01-19 20:11:14 -0500

    updated > error404.component

--- src/app/components/errors/error404/error404.component.ts
+++ src/app/components/errors/error404/error404.component.ts
@@ -1,54 +1,62 @@
-import { Component, Input } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { User } from 'src/app/models/user';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { SharedUsersService } from 'src/app/services/sharedUsers/shared-users.service';
+import { User } from 'src/app/models/user';
 @Component({
   selector: 'app-error404',
   templateUrl: './error404.component.html',
   styleUrls: ['./error404.component.css']
 })
-export class Error404Component {
-  @Input() user: User | null = null;
-  userId: string | null = null;
-
-  constructor(private router:Router,
+export class Error404Component implements OnInit {
+  constructor(
     private userShared:SharedUsersService,
     private authService: AuthService,
-    private route: ActivatedRoute){}
+    private route: ActivatedRoute){
+    }
+
+    // Decorator for using interfaz user
+    @Input() user: User | null = null;
+    userId: string | null = null;
 
     ngOnInit(){
-      this.dataUser()
+      // Initialize the data user
+      this.dataUser();
     }
 
-    isUserLoggedIn(){
-      return this.authService.loggedIn();
+  isUserLoggedIn(){
+    // Get the user logged-in from the authentication service
+    return this.authService.loggedIn();
+  }
+
+  dataUser() {
+    // Get the Id of the logged-in user from the authentication service
+    const loggedInUserId = this.authService.getLoggedInUserId();
+
+    if (loggedInUserId) {
+      // Asign the obtained userId to the component's userId
+      this.userId = loggedInUserId;
+      // Subscribe to changes in Url parameters
+      this.route.paramMap.subscribe(paramMap => {
+
+        // Get the user Id from the Url
+        const id = paramMap.get('id');
+
+        // Check if the Url user Id matches the logged-in user
+        if (id === loggedInUserId) {
+          // Retrieve user data using the userShared service
+          this.userShared.getUser(id).subscribe(data => {
+            // Assign the user data to the 'user' variable
+            this.user = data;
+            // console.log('Data User prfile', data)
+          });
+        } else {
+          // If the IDs are different, log an error
+          console.error('Error: Different or mismatching IDs');
+          // You may also redirect the user to an error page if necessary
+          // this.router.navigate(['/error']);
+        }
+      });
     }
-
-    dataUser() {
-      // Obtiene el ID del usuario logueado desde el servicio de autenticación
-      const loggedInUserId = this.authService.getLoggedInUserId();
-
-      if (loggedInUserId) {
-        // Asignar el userId obtenido al userId del componente
-        this.userId = loggedInUserId;
-        this.route.paramMap.subscribe(paramMap => {
-
-          // Obtiene el ID de usuario de la URL
-          const id = paramMap.get('id');
-
-          // Comprueba si el ID de usuario de la URL coincide con el usuario logueado
-          if (id === loggedInUserId) {
-            this.userShared.getUser(id).subscribe(data => {
-              this.user = data;
-              // console.log('Data User prfile', data)
-            });
-          } else {
-            console.error('Error ids diferentes no coinciden')
-            // this.router.navigate(['/error']);
-          }
-        });
-      }
-    }
-
+  }
 }
