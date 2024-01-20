548d79da4d14d891d4f09e7039d9c3bb10e71963
Brayan <brayantandap@gmail.com>
2024-01-19 20:11:03 -0500

    updated > footer.component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,33 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
 
-  ngOnInit(){
-    this.scrollListener();
+  isFooterHidden: boolean = false;
+
+  ngOnInit() {
+    // Initialize the component when it is created
   }
 
-  scrollListener(){
-    let prevScroll = window.pageYOffset;
+  // HostListener to handle the scroll event
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    const currentScroll = window.pageYOffset;
 
+    // Select the 'footer' element in the DOM
+    const footerElement = document.querySelector('footer');
 
-    window.onscroll =  () => {
-      const currentScroll = window.pageYOffset;
-
-      const footerElement = document.querySelector('footer');
-
-      if (footerElement) {
-        if (prevScroll < currentScroll || currentScroll + window.innerHeight >= document.documentElement.scrollHeight) {
-          footerElement.classList.remove('footer-hidden');
-        } else {
-          footerElement.classList.add('footer-hidden');
-        }
+    if (footerElement) {
+      // Determine whether to hide or show the footer based on scrolling direction
+      if (this.prevScroll < currentScroll || currentScroll + window.innerHeight >= document.documentElement.scrollHeight) {
+        this.isFooterHidden = false;
+      } else {
+        this.isFooterHidden = true;
       }
-      prevScroll = currentScroll;
     }
+
+     // Update the previous scroll position for the next comparison
+    this.prevScroll = currentScroll;
   }
+
+  // Private property to store the previous scroll position
+  private prevScroll: number = 0;
 }
