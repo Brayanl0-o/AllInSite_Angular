936e136205f3d1ffc750bb3a3ece566aa5b262bb
Brayan <brayantandap@gmail.com>
2024-02-06 19:25:34 -0500

    create >add.requirementes.ts

---
+++ src/app/components/add-requirements/add-requirements.component.ts
@@ -0,0 +1,13 @@
+import { Component, Renderer2 } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { VideogamesService } from 'src/app/services/videogames/videogames.service';
+
+@Component({
+  selector: 'app-add-requirements',
+  templateUrl: './add-requirements.component.html',
+  styleUrls: ['./add-requirements.component.css']
+})
+export class AddRequirementsComponent {
+
+
+}
