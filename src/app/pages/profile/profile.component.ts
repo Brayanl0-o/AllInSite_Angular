28552880b07fe811137646e34810a9615dd2def6
Brayan <brayantandap@gmail.com>
2024-02-19 09:30:39 -0500

    updated > profile.component

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -26,96 +26,92 @@ export class ProfileComponent {
    newUserImg = '';
    isEditingImg = false;
 
-    ngOnInit(){
-      this.dataUser();
-      this.userShared.$modal.subscribe((valu) => { this.isModalVisible = valu })
-    }
+  ngOnInit(){
+    this.dataUser();
+    this.userShared.$modal.subscribe((valu) => { this.isModalVisible = valu })
+  }
 
-    dataUser(){
-      const loggedInUserId = this.authService.getLoggedInUserId();
+  dataUser(){
+    const loggedInUserId = this.authService.getLoggedInUserId();
 
-      if(loggedInUserId){
-        this.userId = loggedInUserId;
-        this.route.paramMap.subscribe(paramMap => {
+    if(loggedInUserId){
+      this.userId = loggedInUserId;
+      this.route.paramMap.subscribe(paramMap => {
 
-            const id = paramMap.get('id');
-            this.imageUrl = `${apiBaseUrl}uploads/users/`;
+          const id = paramMap.get('id');
+          this.imageUrl = `${apiBaseUrl}uploads/users/`;
 
-            if (id === loggedInUserId) {
-              this.userShared.getUser(id).subscribe(data => {
-                this.user = data;
-                this.loadDataProfile = false;
-                // console.log('Data User prfile', data)
-              });
-            } else {
-              console.error('No login')
-              // this.router.navigate(['/error']);
-            }
-        })
-      }
-    }
-
-    onFileSelectedUser(event: Event):void {
-      const inputElement = event.target as HTMLInputElement;
-      const file = inputElement?.files?.[0];
-      if (file){
-        this.selectedFile = file;
-      }
-    }
-
-
-    updateDataUser(userImg:File):void{
-      if(!this.user){
-        console.error('Error: No hay datos de actualizacíón')
-        return;
-      }
-      const updatedData = {
-        userImg: this.newUserImg
-      };
-
-      this.userShared.updateUserImg(this.user._id, userImg).subscribe(
-        (response)=> {
-          if(this.user){
-            this.user.userImg = this.newUserImg
-
+          if (id === loggedInUserId) {
+            this.userShared.getUser(id).subscribe(data => {
+              this.user = data;
+              this.loadDataProfile = false;
+              // console.log('Data User prfile', data)
+            });
+          } else {
+            console.error('No login')
+            // this.router.navigate(['/error']);
           }
-          // console.log('Datos act con exito:', response);
-          this.dataUser();
-        },
-        (error) => {
-          console.error('Error al updated', error);
+      })
+    }
+  }
+
+  onFileSelectedUser(event: Event):void {
+    const inputElement = event.target as HTMLInputElement;
+    const file = inputElement?.files?.[0];
+    if (file){
+      this.selectedFile = file;
+    }
+  }
+
+  updateDataUser(userImg:File):void{
+    if(!this.user){
+      console.error('Error: No hay datos de actualizacíón')
+      return;
+    }
+    const updatedData = {
+      userImg: this.newUserImg
+    };
+
+    this.userShared.updateUserImg(this.user._id, userImg).subscribe(
+      (response)=> {
+        if(this.user){
+          this.user.userImg = this.newUserImg
+
         }
-      );
-      this.isEditingImg = false;
-
-    }
-
-    cancelEditImg(){
-      this.newUserImg = this.userImg;
-      this.isEditingImg = false;
-      this.renderer.removeStyle(document.body, 'overflow');
-
-    }
-
-    openModalImg(){
-      this.isEditingImg = true;
-      this.renderer.setStyle(document.body, 'overflow', 'hidden');
-    }
-
-    isModalVisible!: boolean;
-    openModal(user: User | null): void {
-      if (user) {
-        this.isModalVisible = true
-        this.renderer.setStyle(document.body, 'overflow', 'hidden');
-        this.userShared.sendUserData(user);
+        // console.log('Datos act con exito:', response);
+        this.dataUser();
+      },
+      (error) => {
+        console.error('Error al updated', error);
       }
-    }
+    );
+    this.isEditingImg = false;
+  }
 
-    showContentOne = true;
-    showContentTwo = false;
-    toggleContent() {
-      this.showContentOne = !this.showContentOne;
-      this.showContentTwo = !this.showContentTwo;
-    }
+  cancelEditImg(){
+    this.newUserImg = this.userImg;
+    this.isEditingImg = false;
+    this.renderer.removeStyle(document.body, 'overflow');
+  }
 
+  openModalImg(){
+    this.isEditingImg = true;
+    this.renderer.setStyle(document.body, 'overflow', 'hidden');
+  }
+
+  isModalVisible!: boolean;
+  openModal(user: User | null): void {
+    if (user) {
+      this.isModalVisible = true
+      this.renderer.setStyle(document.body, 'overflow', 'hidden');
+      this.userShared.sendUserData(user);
+    }
+  }
+
+  showContentOne = true;
+  showContentTwo = false;
+  toggleContent() {
+    this.showContentOne = !this.showContentOne;
+    this.showContentTwo = !this.showContentTwo;
+  }
 }
