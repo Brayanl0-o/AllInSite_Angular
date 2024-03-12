e0344196e773d196685ba31e42cc380b52d915c5
Brayan <brayantandap@gmail.com>
2024-03-12 03:03:59 -0500

    refactored > user

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from 'src/app/modules/user/pages/profile/profile.component';
 
 
 const routes: Routes = [
+  { path: 'profile/:id', component: ProfileComponent},
 
 ];
 
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { userRoutingModule } from './user-routing.module';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { MatIconModule } from '@angular/material/icon';
+import { UpdateUserComponent } from './components/update-user/update-user.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { LayoutModule } from 'src/app/layout/layout.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileComponent,
+    UpdateUserComponent
+  ],
   imports: [
+    LayoutModule,
+    SharedModule,
     CommonModule,
-    userRoutingModule
+    userRoutingModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+
   ]
 })
 export class UserModule { }
