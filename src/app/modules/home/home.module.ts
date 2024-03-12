e1d991e360aab22b9511985e86c87ee52cef01ab
Brayan <brayantandap@gmail.com>
2024-03-12 03:02:44 -0500

    refactored > home

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { homeRoutingModule } from './home-routing.module';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SliderGamesComponent } from 'src/app/modules/home/components/slider-games/slider-games.component';
+import { LayoutModule } from 'src/app/layout/layout.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    SliderGamesComponent
+  ],
   imports: [
+    LayoutModule,
     CommonModule,
-    homeRoutingModule
+    homeRoutingModule,
+    RouterModule,
+
   ]
 })
 export class HomeModule { }
