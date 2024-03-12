db2d357c58f5296744dcebe97cdac507512b54b1
Brayan <brayantandap@gmail.com>
2024-03-12 03:01:22 -0500

    refactored > videogames

--- src/app/modules/videogames/videogames.module.ts
+++ src/app/modules/videogames/videogames.module.ts
@@ -1,14 +1,44 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { MatIconModule } from '@angular/material/icon';
+
+import { SharedModule } from 'src/app/shared/shared.module';
+import { LayoutModule } from 'src/app/layout/layout.module';
+
 import { videogamesRoutingModule } from './videogames-routing.module';
-
-
-
+import { VideogamesComponent } from 'src/app/modules/videogames/pages/videogames/videogames.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { HeaderSearchComponent } from './components/header-search/header-search.component';
+import { AddVideogameComponent } from './components/add-videogame/add-videogame.component';
+import { DetailsGameComponent } from './pages/details-game/details-game.component';
+import { AddRequirementsComponent } from './components/add-requirements/add-requirements.component';
+import { EditVideogameComponent } from './components/edit-videogame/edit-videogame.component';
+import { LoadingScreenComponent } from './components/loading-screen/loading-screen.component';
 @NgModule({
-  declarations: [],
+  declarations: [
+    VideogamesComponent,
+    DetailsGameComponent,
+    EditVideogameComponent,
+    AddRequirementsComponent,
+    AddVideogameComponent,
+    AsideComponent,
+    HeaderSearchComponent,
+    LoadingScreenComponent,
+
+  ],
   imports: [
+    LayoutModule,
+    SharedModule,
+    MatIconModule,
     CommonModule,
-    videogamesRoutingModule
+    videogamesRoutingModule,
+    // AppModule,
+    NgxPaginationModule,
+    FormsModule,
+    ReactiveFormsModule,
+
   ]
 })
 export class VideogamesModule { }
