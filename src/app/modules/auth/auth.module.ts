735f05ea1a62b91a89f743fedf3da5155b2050ed
Brayan <brayantandap@gmail.com>
2024-03-13 23:42:32 -0500

    updated > auth.module

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -4,17 +4,23 @@ import { CommonModule } from '@angular/common';
 import { authRoutingModule } from './auth-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './pages/register/register.component';
+import { RouterModule } from '@angular/router';
+// import { ModalLoginComponent } from '../../shared/components/modal-login/modal-login.component';
+import { LayoutModule } from 'src/app/layout/layout.module';
+
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
+import { CoreModule } from 'src/app/core/core.module';
 import { ModalLoginComponent } from './components/modal-login/modal-login.component';
 @NgModule({
   declarations: [
     RegisterComponent,
-    ModalLoginComponent
+    ModalLoginComponent,
   ],
   imports: [
+    // CoreModule,
+    // LayoutModule,
     MatIconModule,
     CommonModule,
     RouterModule,
