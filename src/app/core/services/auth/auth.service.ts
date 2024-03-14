cf4e0926458287c9f6ecd6239cee2dc51b703232
Brayan <brayantandap@gmail.com>
2024-03-13 23:40:35 -0500

    updated > auth.service.ts

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -18,12 +18,73 @@ export class AuthService {
   constructor(
     private http: HttpClient,
     private router: Router,
-    // private afAuth: AngularFireAuth
-    ) { }
+    ) {
+      const token = localStorage.getItem('token');
+      if(token){
+        this.user = this.parseToken(token);
+        this.isLoggedIn$.next(true);
+      }
+     }
+     private parseToken(token: string): User {
+      const payload = JSON.parse(atob(token.split('.')[1]))
+      return  payload.id
+     }
+  isLoggedIn$ = new BehaviorSubject<boolean>(false);
+  user: User | null = null;
 
+  public login(user: any) {
+    return this.http.post<any>(this.apiUrl + 'auth/login', user).pipe(
+      tap((response) => {
+        // console.log('Respuesta del servidor:', response);
+        const token = response.token;
+        // console.log('Contenido del payload del token decodificado:', atob(token.split('.')[1]));
 
+        localStorage.setItem('token', token)
+
+        this.isLoggedIn$.next(true);
+      })
+    )
+  }
   gettoken() {
     return localStorage.getItem('token')
+  }
+  isLoggedIn(): boolean {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      const payload = JSON.parse(atob(token.split('.')[1]));
+      const expiration = new Date(payload.exp * 1000); // Convierte el tiempo de expiración a una fecha
+      const now = new Date();
+
+      return now < expiration;
+
+    }
+    console.log(token)
+
+    return false;
+  }
+  getLoggedInUserId(): string | null {
+    const token = this.gettoken();
+    if (token) {
+      const payload = JSON.parse(atob(token.split('.')[1]));
+
+      if (payload.id) {
+        console.log('payload id from getLoggedInUserId:', payload.id)
+        return payload.id;
+      }
+    }
+    return 'null';
+  }
+  getLoggedUserRole(): string {
+    const token = this.gettoken();
+    if (token) {
+      const payload = JSON.parse(atob(token.split('.')[1]));
+
+      if (payload.roles) {
+        return payload.roles;
+      }
+    }
+    return 'null';
   }
 
   public signUp(userData: any, userImg:any): Observable<any> {
@@ -40,9 +101,7 @@ export class AuthService {
         formData.append('userImg', userImg);
     }
 
-    // const headers = new HttpHeaders().set('Content-Type', 'multipart/form-data');
-    // const headers = new HttpHeaders();
-    console.log('userData service', userData)
+    // console.log('userData service', userData)
     return this.http.post(this.apiUrl + 'auth/signup', formData, {  observe: 'response' }).pipe(
       catchError((error: any) => {
         console.error('Error en la solicitud(servicio) de registro signUp: ', error);
@@ -51,62 +110,6 @@ export class AuthService {
     );;
   }
 
-  public login(user: any) {
-    return this.http.post<any>(this.apiUrl + 'auth/login', user).pipe(
-      tap((response) => {
-        // console.log('Respuesta del servidor:', response);
-        const token = response.token;
-        // console.log('Contenido del payload del token decodificado:', atob(token.split('.')[1]));
-
-        localStorage.setItem('token', response.token)
-
-        this.isLoggedIn$.next(true);
-      })
-    )
-
-  }
-
-  isLoggedIn$ = new BehaviorSubject<boolean>(false);
-  loggedIn(): boolean {
-    const token = localStorage.getItem('token');
-
-    if (token) {
-      const payload = JSON.parse(atob(token.split('.')[1]));
-      const expiration = new Date(payload.exp * 1000); // Convierte el tiempo de expiración a una fecha
-      const now = new Date();
-      return now < expiration;
-    }
-    return false;
-  }
-
-  logout() {
-    localStorage.removeItem('token')
-    this.router.navigate(['/home'])
-  }
-
-  getLoggedInUserId(): string | null {
-    const token = this.gettoken();
-    if (token) {
-      const payload = JSON.parse(atob(token.split('.')[1]));
-
-      if (payload.id) {
-        return payload.id;
-      }
-    }
-    return 'null';
-  }
-
-  getLoggedUserRole(): string {
-    const token = this.gettoken();
-    if (token) {
-      const payload = JSON.parse(atob(token.split('.')[1]));
-
-      if (payload.roles) {
-        return payload.roles;
-      }
-    }
-    return 'null';
-  }
 
   getUserById(userId: string): Observable<User> {
 
@@ -121,6 +124,13 @@ export class AuthService {
       }),
     };
     return this.http.get<User>(url, httpOptions);
+  }
+  logout() {
+    localStorage.removeItem('token');
+    this.isLoggedIn$.next(false);
+
+    this.router.navigate(['/home'])
+    console.log('execute logout')
   }
 
 }
