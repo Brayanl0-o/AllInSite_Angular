90419c0725ea536a2e1b56ed50d8187f6582d8a7
Brayan <brayantandap@gmail.com>
2024-03-20 22:05:09 -0500

    feat > profile.component.ts

--- src/app/modules/user/pages/profile/profile.component.ts
+++ src/app/modules/user/pages/profile/profile.component.ts
@@ -108,4 +108,8 @@ export class ProfileComponent {
     this.showContentOne = !this.showContentOne;
     this.showContentTwo = !this.showContentTwo;
   }
+  capitalizeFirstLetter(value: string): string {
+    if (!value) return '';
+    return value.charAt(0).toUpperCase() + value.slice(1);
+  }
 }
