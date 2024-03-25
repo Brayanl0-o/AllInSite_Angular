2d9ab1d37ff30e41b8917e498f121bfaa7deda64
Brayan <brayantandap@gmail.com>
2024-03-25 12:38:46 -0500

    feat

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthGuard } from './guards/auth/auth.guard';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
-  ]
+  ],
+  providers: [AuthGuard]
 })
 export class CoreModule { }
