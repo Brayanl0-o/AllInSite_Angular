0a99133e375c8c5272d5d3987cf5f6ecd45182a3
Brayan <brayantandap@gmail.com>
2024-04-24 16:03:33 -0500

    feat >songs.service

--- src/app/core/services/music/songs/songs.service.ts
+++ src/app/core/services/music/songs/songs.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Song } from 'src/app/core/models/song';
@@ -9,11 +9,28 @@ import { Song } from 'src/app/core/models/song';
 })
 export class SongsService {
   private apiUrl = environment.apiUrl;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.$songUpdateDetails.subscribe((value: boolean) => {
+    console.log('Estado actual de songUpdateDetails:', value);
+  });
+  }
+
+  $songDetails = new EventEmitter<any>();
+  $songUpdateDetails = new EventEmitter<any>();
 
 
-  public getSong(): Observable<Song[]>{
+
+  public getSongs(): Observable<Song[]>{
     const url = `${this.apiUrl}songs/`;
     return this.http.get<Song[]>(url)
+  }
+  public getSong(songId: string ): Observable<Song>{
+    const url = `${this.apiUrl}songs/${songId}`;
+    return this.http.get<Song>(url)
+  }
+  public updateSong(songId: string): Observable<Song>{
+    const url = `${this.apiUrl}songs/update/${songId}`
+    return this.http.get<Song>(url)
+
   }
 }
