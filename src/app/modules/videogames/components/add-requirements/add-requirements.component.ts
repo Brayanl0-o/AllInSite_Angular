1f94c358b55c71d8b3421bff7ea0f6b6eb2767f3
Brayan <brayantandap@gmail.com>
2024-05-01 18:43:16 -0500

    feat > add-requirements.compoennt

--- src/app/modules/videogames/components/add-requirements/add-requirements.component.ts
+++ src/app/modules/videogames/components/add-requirements/add-requirements.component.ts
@@ -1,5 +1,5 @@
-import { Component, Renderer2 } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { GameRequirements } from 'src/app/core/models/game';
 import { VideogamesService } from 'src/app/core/services/videogames/videogames.service';
@@ -10,17 +10,17 @@ import { VideogamesService } from 'src/app/core/services/videogames/videogames.s
   styleUrls: ['./add-requirements.component.css']
 })
 export class AddRequirementsComponent {
-  constructor(private videoGamesService: VideogamesService,
-    private route: ActivatedRoute,
-    private renderer: Renderer2,
-    private fb: FormBuilder)
-    {this.errorResponseMessage = '';}
-
+  gameRequirements: GameRequirements | null = null;
   contactForm!: FormGroup;
   errorResponseMessageForm = '';
   errorResponseMessage = '';
   isLoading = false;
   gameId = '';
+
+  constructor(private videoGamesService: VideogamesService,
+    private route: ActivatedRoute,
+    private fb: FormBuilder)
+    {}
 
   ngOnInit():void{
     this.contactForm = this.initFrom();
@@ -37,14 +37,20 @@ export class AddRequirementsComponent {
       this.isLoading = true;
 
       if(this.contactForm.valid){
+        const { ramValue, ramUnit, sizeValue, sizeUnit, ...rest } = this.contactForm.value;
+        const sizeGame = { value: sizeValue, unit: sizeUnit};
+        const ramGame = { value: ramValue, unit: ramUnit};
 
-        const gameRequeriments: GameRequirements = { ...this.contactForm.value, gameId: this.gameId };
-        this.videoGamesService.updateRequirements( gameRequeriments).subscribe(
+        const gameRequirements = { ...rest, sizeGame, ramGame, gameId: this.gameId };
+        // const gameRequirements: GameRequirements = { ...this.contactForm.value, gameId: this.gameId };
+        this.videoGamesService.updateRequirements(gameRequirements).subscribe(
           (response) => {
+            console.log(response)
             this.isLoading = false;
             window.location.reload();
           },
           (error) => {
+            this.isLoading = false;
             console.error('Error al actualizar los datos:', error);
           }
         )
@@ -60,8 +66,10 @@ export class AddRequirementsComponent {
     initFrom(): FormGroup{
       return this.fb.group({
         platform: ['',[ Validators.maxLength(60)]],
-        sizeGame: ['', [ Validators.maxLength(5), this.numbersOnlyValidator]],
-        ramGame: ['',[ Validators.maxLength(3), this.numbersOnlyValidator]],
+        sizeValue: ['', [Validators.maxLength(4), this.numbersOnlyValidator]],
+        sizeUnit: ['KB'],
+        ramValue: ['', [Validators.maxLength(3), this.numbersOnlyValidator]],
+        ramUnit: ['MB'],
         processorGame: ['',[ Validators.maxLength(200)]],
         graphGame: ['',[ Validators.maxLength(200)]],
 
