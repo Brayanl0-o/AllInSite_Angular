da94cbb3c247be4e9099e07502c9a087b303ecaf
Brayan <brayantandap@gmail.com>
2024-05-09 23:47:20 -0500

    updated > music-audio-player.component

--- src/app/modules/music/components/music-audio-player/music-audio-player.component.ts
+++ src/app/modules/music/components/music-audio-player/music-audio-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-music-audio-player',
@@ -8,11 +8,10 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class MusicAudioPlayerComponent {
   isPlaying: boolean = false;
   @ViewChild('audioPlayer') audioPlayerRef!: ElementRef<HTMLAudioElement>;
-
-  constructor() {}
-
+  @ViewChild('slider_bar::before') progressElement: ElementRef<HTMLDivElement> | undefined;
+  constructor(private renderer: Renderer2) {}
   toggleAudio() {
-    const audioPlayer = this.audioPlayerRef.nativeElement;
+    const audioPlayer = this.audioPlayerRef?.nativeElement;
     if (audioPlayer) {
       if (this.isPlaying) {
         audioPlayer.pause();
@@ -20,12 +19,34 @@ export class MusicAudioPlayerComponent {
         audioPlayer.play();
       }
       this.isPlaying = !this.isPlaying;
+
     }
   }
+
+  formatTime(seconds: number): string {
+    // const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+    return `${this.pad(minutes)}:${this.pad(remainingSeconds)}`;
+  }
+
+  private pad(value: number): string {
+    return value.toString().padStart(2, '0');
+  }
+  seekTo(time: number) {
+    const audioPlayer = this.audioPlayerRef.nativeElement;
+
+    audioPlayer.currentTime = time;
+  }
+
+
 
   onAudioEnded() {
     this.isPlaying = false;
   }
+  currentTime: string = '0:00:00';
+  duration: string = '0:00:00';
+
 
   ngAfterViewInit() {
     const audioPlayer = this.audioPlayerRef.nativeElement;
@@ -40,5 +61,13 @@ export class MusicAudioPlayerComponent {
         this.isPlaying = false;
       });
     }
+    if (audioPlayer) {
+      setInterval(() => {
+        if (!audioPlayer.paused) {
+          this.currentTime = this.formatTime(audioPlayer.currentTime);
+        }
+      }, 1000); // Actualizar cada segundo
+    }
   }
+
 }
