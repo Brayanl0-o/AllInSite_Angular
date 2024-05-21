f8b452ad36e69f92c25add7d6dbc47b6e0949823
Brayan <brayantandap@gmail.com>
2024-05-21 15:43:45 -0500

    feat > core.module.ts

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthGuard } from './guards/auth/auth.guard';
+import { SafeUrlPipe } from './pipes/safe-url.pipe';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SafeUrlPipe
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    SafeUrlPipe
   ],
   providers: [AuthGuard]
 })
