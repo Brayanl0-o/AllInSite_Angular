a0d844517f6a72af8ad6f50a76acfd01b8f98fb0
Brayan <brayantandap@gmail.com>
2024-09-12 01:50:34 -0500

    fix

--- src/app/modules/music/components/music-modal-playlist/music-modal-playlist.component.ts
+++ src/app/modules/music/components/music-modal-playlist/music-modal-playlist.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Playlist } from 'src/app/core/models/song';
@@ -11,39 +11,104 @@ import { SongsService } from 'src/app/core/services/music/songs/songs.service';
   templateUrl: './music-modal-playlist.component.html',
   styleUrls: ['./music-modal-playlist.component.css']
 })
-export class MusicModalPlaylistComponent {
+export class MusicModalPlaylistComponent implements OnInit {
   visibilityForm = false;
   playlists: Playlist[] = [];
   isLoading = false;
   contactForm!: FormGroup;
   errorResponseMessageForm = '';
-
   songId!: string;
   selectedPlaylistId!: string;
   playlistForm!: FormGroup;
 
   constructor(
-    private songService: SongsService, private fb: FormBuilder,
-    private authService: AuthService,  private route: ActivatedRoute)
-    {}
+    private songService: SongsService,
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(){
-    this.route.paramMap.subscribe(params => {
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
       this.songId = params.get('songId') || '';
-      console.log('get songId',this.songId);
     });
-    // this.loadPlaylists();
-    // this.playlistForm =  this.initFromPlaylistArray();
-    this.contactForm =  this.initFrom();
-    this.songService.getPlaylists().subscribe((value: Playlist[]) => {
-      this.playlists = value;
-    })
+    
+    // Inicializa el formulario
+    this.contactForm = this.initFrom();
+    this.playlistForm = this.initFromPlaylistArray();
+
+    // Carga las playlists y configura el FormArray
+    this.loadPlaylists();
   }
 
+  initFromPlaylistArray(): FormGroup {
+    return this.fb.group({
+      playlists: this.fb.array([])
+    });
+  }
 
+  loadPlaylists(): void {
+    this.songService.getPlaylists().subscribe((playlists) => {
+      this.playlists = playlists;
+  
+      // Limpia y configura los controles de FormArray
+      const formArray = this.playlistForm.get('playlists') as FormArray;
+      formArray.clear();
+  
+      // Itera sobre las playlists para configurar el FormArray con el estado correcto
+      playlists.forEach((playlist) => {
+        // Chequea si la canción actual está en la playlist
+        const isSongInPlaylist = playlist.songs.includes(this.songId);
+        formArray.push(new FormControl(isSongInPlaylist));
+      });
+    });
+  }
 
+  get playlistArray(): FormArray {
+    return this.playlistForm.get('playlists') as FormArray;
+  }
 
+  onCheckboxChange(index: number): void {
+    const formArray: FormArray = this.playlistArray;
+    const playlistId = this.playlists[index]._id;
 
+    if (formArray.controls[index].value) {
+      this.addSongToPlaylist(playlistId);
+    } else {
+      this.removeSongFromPlaylist(playlistId);
+    }
+  }
+
+  addSongToPlaylist(playlistId: string): void {
+    if (this.songId && playlistId) {
+      this.isLoading = true;
+      this.songService.addSongToPlaylist(playlistId, this.songId).subscribe(
+        (response) => {
+          this.isLoading = false;
+        },
+        (error) => {
+          this.isLoading = false;
+          this.errorResponseMessageForm = 'Error adding song to playlist';
+        }
+      );
+    }
+  }
+
+  removeSongFromPlaylist(playlistId: string): void {
+    if (this.songId && playlistId) {
+      this.isLoading = true;
+      this.songService.removeSongFromPlaylist(playlistId, this.songId).subscribe(
+        (response) => {
+          this.isLoading = false;
+        },
+        (error) => {
+          this.isLoading = false;
+          console.error('Error removing song from playlist', error);
+          this.errorResponseMessageForm = 'Error removing song from playlist';
+        }
+      );
+    }
+  }
   onFormSubmit(){
     this.isLoading = true;
 
@@ -105,63 +170,4 @@ export class MusicModalPlaylistComponent {
     this.songService.$modalPlaylist.emit(false);
   }
 }
- // initFromPlaylistArray(): FormGroup{
-  //   return this.fb.group({
-  //     playlists: new FormArray([])
-  //   });
-  // }
-  // loadPlaylists(): void {
-  //   this.songService.getPlaylists().subscribe(playlists => {
-  //     this.playlists = playlists;
-  //     this.playlists.forEach(() => this.playlistArray.push(new FormControl(false)));
-  //   });
-  // }
 
-  // get playlistArray(): FormArray {
-  //   return this.playlistForm.get('playlists') as FormArray;
-  // }
-
-  // onCheckboxChange(index: number): void {
-  //   const formArray: FormArray = this.playlistArray;
-  //   const playlistId = this.playlists[index]._id;
-
-  //   if (formArray.controls[index].value) {
-  //     this.addSongToPlaylist(playlistId);
-  //   } else {
-  //     this.removeSongFromPlaylist(playlistId);
-  //   }
-  // }
-
-  // addSongToPlaylist(playlistId: string): void {
-  //   if (this.songId && playlistId) {
-  //     this.isLoading = true;
-  //     this.songService.addSongToPlaylist(playlistId, this.songId).subscribe(
-  //       (response) => {
-  //         this.isLoading = false;
-  //         console.log('Song added to playlist', response);
-  //       },
-  //       (error) => {
-  //         this.isLoading = false;
-  //         console.error('Error adding song to playlist', error);
-  //         this.errorResponseMessage = 'Error adding song to playlist';
-  //       }
-  //     );
-  //   }
-  // }
-
-  // removeSongFromPlaylist(playlistId: string): void {
-  //   if (this.songId && playlistId) {
-  //     this.isLoading = true;
-  //     this.songService.removeSongFromPlaylist(playlistId, this.songId).subscribe(
-  //       (response) => {
-  //         this.isLoading = false;
-  //         console.log('Song removed from playlist', response);
-  //       },
-  //       (error) => {
-  //         this.isLoading = false;
-  //         console.error('Error removing song from playlist', error);
-  //         this.errorResponseMessage = 'Error removing song from playlist';
-  //       }
-  //     );
-  //   }
-  // }
